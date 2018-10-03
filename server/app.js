1c5a8b40c7f6a8a830ccd53aa0a6631bf0f17ba5
Eric Scott <scottzillaster@gmail.com>
2018-10-03 15:33:48 -0500

    fixied please?

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-app.use(express.static('../client/build'))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(express.static('/client/build'))
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
