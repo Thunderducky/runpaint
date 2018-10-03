584182d56aa7ea8f902a2172ac83c13343fc47c8
Eric Scott <scottzillaster@gmail.com>
2018-10-03 15:13:34 -0500

    more production fixes

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const chalk = require('chalk')
+const path = require('path')
 const app = require('./server/app')
 const express = require('express')
 const PORT = process.env.PORT || 3001
 app.use(express.static('client/build'))
+app.use(function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 app.listen(PORT, () =>
   console.log(chalk.bgBlue(`Now listening on PORT :${PORT}`)) //eslint-disable-line no-console
 )
