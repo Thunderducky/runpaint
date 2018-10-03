04ad2e77f58a7610e7c52be3d25d2822d16db2f7
Eric Scott <scottzillaster@gmail.com>
2018-10-03 15:03:57 -0500

    getting ready for deploy

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const chalk = require('chalk')
 const app = require('./server/app')
-
+const express = require('express')
 const PORT = process.env.PORT || 3001
+app.use(express.static('client/build'))
 app.listen(PORT, () =>
   console.log(chalk.bgBlue(`Now listening on PORT :${PORT}`)) //eslint-disable-line no-console
 )
