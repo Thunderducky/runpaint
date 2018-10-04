68057608aa7fa5c4e798cf91527abc841cd8c076
Eric Scott <scottzillaster@gmail.com>
2018-10-04 16:00:05 -0500

    fixes #10, adds multi-topic support to pubsub, so we can track multiple topics at once

--- client/src/modules/pubsub.js
+++ client/src/modules/pubsub.js
@@ -8,25 +8,48 @@ const makePubSub = () => {
     topics: {}
   }
 
-  obj.publish = (topic, _msg) => {
+  obj.publish = (_topic, _msg) => {
     // this could have side effects, like accidentally
     // freezing things, but most importantly
     // it keeps subscribers from accidentally changing things
     // I'd rather have objects having to make copies
     // than worry about accidentally changing properties
     const msg = Object.freeze(_msg)
-    if(obj.topics[topic]){
-      obj.topics[topic].forEach(sub => sub(msg, topic))
+    const topicSets = _topic.split('.')
+    while(topicSets.length > 0){
+      const topic = topicSets.join('.')
+      if(obj.topics[topic]){
+        obj.topics[topic].forEach(sub => sub(msg, topic))
+      }
+      topicSets.pop()
     }
+
   }
   obj.subscribe = (topic, fnListener) => {
     if((typeof fnListener) !== 'function'){
       throw Error('fnListener must be a function')
     }
-
+    // don't do it this way
+    const id = genId()
+    const wrapper = (msg, topic) => { fnListener(msg, topic)}
     obj.topics[topic] = obj.topics[topic] || []
-    fnListener._id = genId() // mostly for debugging purposes
-    obj.topics[topic].push(fnListener)
+    wrapper._id = id // mostly for debugging purposes
+    obj.topics[topic].push(wrapper)
+    return id
+  }
+  obj.subscriberCount = (topic) => {
+    const subs = obj.topics[topic]
+    if(!Array.isArray(subs)){
+      return 0
+    } else {
+      return subs.length
+    }
+  }
+  obj.unsubscribe = (topic, fnId) => {
+    if(obj.subscriberCount(topic) === 0){
+      throw Error('no subscribers on this topic')
+    }
+    obj.topics[topic] = obj.topics[topic].filter(fn => fn._id != fnId)
   }
 
   return obj
--- client/src/modules/pubsub.test.js
+++ client/src/modules/pubsub.test.js
@@ -1,7 +1,10 @@
 import { makePubSub } from './pubsub'
 
-const Pubsub1 = makePubSub()
+let Pubsub1 = makePubSub()
 describe('Testing Pubsub', () => {
+  beforeEach(() => {
+    Pubsub1 = makePubSub()
+  })
   test('normal-subscribing-is-errorless', () => {
     expect( () =>
       Pubsub1.subscribe('test.message', (msg, topic) => {}) //eslint-disable-line no-unused-vars
@@ -11,10 +14,10 @@ describe('Testing Pubsub', () => {
     expect(() => Pubsub1.subscribe('test.message')).toThrow()
   })
   test('single-subscriber-should-receive-publications', () => {
-    Pubsub1.subscribe('test.message.receivable', (msg) => {
+    Pubsub1.subscribe('test.message.receivable.single', (msg) => {
       expect(msg).toBe('test1')
     })
-    Pubsub1.publish('test.message.receivable', 'test1')
+    Pubsub1.publish('test.message.receivable.single', 'test1')
   })
   test('all-subscribers-should-receive-publications', () => {
     Pubsub1.subscribe('test.message.receivable.multi', (msg) => {
@@ -38,4 +41,51 @@ describe('Testing Pubsub', () => {
       Pubsub1.publish('test.message.no.modifying.messages', {test:false})
     }).toThrow()
   })
+
+  test('that-it-supports-subtopic-supertopics', () => {
+    expect.assertions(3)
+    Pubsub1.subscribe('test.message.supertopic', msg => {
+      // should fire twice
+      expect(msg.received).toBe(true)
+    })
+    Pubsub1.subscribe('test.message.supertopic.subtopic', msg => {
+      // should fire once
+      expect(msg.received).toBe(true)
+    })
+    Pubsub1.publish('test.message.supertopic.subtopic', {received: true})
+    Pubsub1.publish('test.message.supertopic', { received: true})
+  })
+
+  test('that-it-supports-unsubscribing', () => {
+    expect.assertions(1)
+    const subId = Pubsub1.subscribe('test.message.unsubscribe.soon', msg => {
+      expect(msg.received).toBe(true)
+    })
+    Pubsub1.publish('test.message.unsubscribe.soon', { received:true})
+
+    Pubsub1.unsubscribe('test.message.unsubscribe.soon', subId)
+    Pubsub1.publish('test.message.unsubscribe.soon', { received:true})
+
+  })
+
+  test('that-the-topic-is-passed-along-as-well', () => {
+    expect.assertions(1)
+    Pubsub1.subscribe('test.message.topic', (msg,topic) => {
+      expect(topic).toBe(test.message.topic)
+    })
+    Pubsub1.publish('test.message.topic', { notbeing:'tested'})
+
+  })
+  test('that-we-can-see-subscriber-count', () => {
+    const NOOP = () => {}
+    Pubsub1.subscribe('test.subscriber.count', NOOP)
+    Pubsub1.subscribe('test.subscriber.count', NOOP)
+    const subId = Pubsub1.subscribe('test.subscriber.count', NOOP)
+    expect(Pubsub1.subscriberCount('test.subscriber.count')).toBe(3)
+    Pubsub1.unsubscribe('test.subscriber.count', subId)
+    expect(Pubsub1.subscriberCount('test.subscriber.count')).toBe(2)
+
+  })
+
+  // TODO: Add a test for subscriber counts
 })
