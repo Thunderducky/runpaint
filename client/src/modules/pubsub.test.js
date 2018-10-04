e5ebaf7ecdd0b3107005a82524d2a0dc99357b4a
Eric Scott <scottzillaster@gmail.com>
2018-10-04 16:32:07 -0500

    fixed

--- client/src/modules/pubsub.test.js
+++ client/src/modules/pubsub.test.js
@@ -38,4 +38,48 @@ describe('Testing Pubsub', () => {
       Pubsub1.publish('test.message.no.modifying.messages', {test:false})
     }).toThrow()
   })
+
+  test('that-it-supports-subtopic-supertopics', () => {
+    expect.assertions(3)
+    Pubsub1.subscribe('test.message.supertopic', msg => {
+      // should fire twice
+      expect(msg.received).toBe(true)
+    })
+    Pubsub1.subscribe('test.message.supertopic.subtopic', msg => {
+      // should fire once
+      expect(msg.received).toBe(true)
+    })
+    Pubsub1.publish('test.message.supertopic.subtopic', {received: true})
+    Pubsub1.publish('test.message.supertopic', { received: true})
+  })
+
+  test('that-it-supports-unsubscribing', () => {
+    expect.assertions(1)
+    const subId = Pubsub1.subscribe('test.message.unsubscribe.soon', msg => {
+      expect(msg.received).toBe(true)
+    })
+    Pubsub1.publish('test.message.unsubscribe.soon', { received:true})
+
+    Pubsub1.unsubscribe('test.message.unsubscribe.soon', subId)
+    Pubsub1.publish('test.message.unsubscribe.soon', { received:true})
+
+  })
+
+  test('that-the-topic-is-passed-along-as-well', () => {
+    expect.assertions(1)
+    Pubsub1.subscribe('test.message.topic', (msg,topic) => {
+      expect(topic).toBe(test.message.topic)
+    })
+    Pubsub1.publish('test.message.topic', { notbeing:'tested'})
+
+  })
+  test('that-we-can-see-subscriber-count', () => {
+    const NOOP = () => {}
+    Pubsub1.subscribe('test.subscriber.count', NOOP)
+    Pubsub1.subscribe('test.subscriber.count', NOOP)
+    const subId = Pubsub1.subscribe('test.subscriber.count', NOOP)
+    expect(Pubsub1.subscriberCount('test.subscriber.count')).toBe(3)
+    Pubsub1.unsubscribe('test.subscriber.count', subId)
+    expect(Pubsub1.subscriberCount('test.subscriber.count')).toBe(2)
+  })
 })
