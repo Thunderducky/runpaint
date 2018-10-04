94ef97f8cff15efd0a0e59e157537eda80e70658
Eric Scott <scottzillaster@gmail.com>
2018-10-03 19:43:45 -0500

    added ssl express

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const secure = require('ssl-express-www')
 const app = express()
+
+app.use(secure)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
